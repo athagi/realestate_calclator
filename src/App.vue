5366d243bca46d4a4db40f0339f8a07dc5615d73
athagi <at.hgt.dev@gmail.com>
2019-01-03 20:57:10 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <myheader></myheader>
+    <test></test>
+    <p v-if="msg.length > 0">
+      {{msg}}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+  </div>
+</template>
+
+<script>
+import myheader from './components/myheader'
+import test from './components/test'
+
+export default {
+  components: {
+    myheader,
+    test
+  },
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1 v-if="msg.length > 0">{{ msg }}</h1>
+    <p v-else>no text</p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Hello world'
+    }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
